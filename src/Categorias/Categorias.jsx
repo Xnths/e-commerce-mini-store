0bac48579737f63f5a0f488918f9c4582bb2be3a
Jonathas Castilho <jonathascastilhoes@gmail.com>
2022-03-05 21:30:54 -0300

    Adicionando ver mais opções

--- src/Categorias/Categorias.jsx
+++ src/Categorias/Categorias.jsx
@@ -1,17 +1,25 @@
 import React, {Component} from "react";
+import { Link } from "react-router-dom";
 import categoriasApi from "../categorias"
 
 class Categorias extends Component {
+    getCategorias() {
+        const limite = categoriasApi.retorno.categorias.length;
+        let element = [];
+
+        for(let i = 0; i < limite; i++) {
+            element.push(<li>{categoriasApi.retorno.categorias[i].categoria.descricao}</li>)
+        }
+
+        element.push(<li><Link to="/categorias">Ver mais categorias</Link></li>)
+
+        return element
+    }
     render() {
         return (
             <nav className="categorias__navegacao">
                 <ul className="categorias__container">
-                    {categoriasApi.retorno.categorias.map(value => {
-                        let categoria = value.categoria;
-                        return (
-                            <li className="categorias__item" key={categoria.id}>{categoria.descricao}</li>
-                        )
-                    })}
+                    {this.getCategorias()}
                 </ul>
             </nav>
         )
