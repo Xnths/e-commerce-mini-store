493bbd6b48843a8361eac5a7f53c7c5d65e75250
Jonathas Castilho <jonathascastilhoes@gmail.com>
2022-03-06 15:00:04 -0300

    Exibição de preço com duas casas decimais

--- src/Produto/Produto.jsx
+++ src/Produto/Produto.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import produtosApi from "../produtos";
 
 
+
 function getCodigoProduto() {
     let urlParams = global.window.location.search
     let codigoProduto = new URLSearchParams(urlParams).get('cod')
@@ -29,7 +30,7 @@ class Produto extends Component {
                         imagem: [...produto.imagem.map(value => value.link)],
                         descricao: produto.descricao,
                         categoria: [...produto.produtoLoja.categoria.map(value => value.descricao)],
-                        preco: produto.preco,
+                        preco: Number(produto.preco).toFixed(2),
                         descricaoComplementar: produto.descricaoComplementar
                     }
                 })
@@ -42,17 +43,25 @@ class Produto extends Component {
     }
 
     componentWillUnmount() {
+        this.setState({})
     }
 
     render() {
         return (
             <div>
-                <img src={this.state.produto.imagem}/>
-                <h2>{this.state.produto.descricao}</h2>
-                <p>{this.state.produto.categoria[0]}</p>
-                <p>{this.state.produto.preco}</p>
-                <p>{this.state.produto.descricaoComplementar}</p>
-                <button>Adicionar ao carrinho</button>
+                <section>
+                    <img src={this.state.produto.imagem}/>
+                    <h2>{this.state.produto.descricao}</h2>
+                    <p>{this.state.produto.categoria[0]}</p>
+                    <p>{this.state.produto.preco}</p>
+                    <p>{this.state.produto.descricaoComplementar}</p>
+                    <button>Adicionar ao carrinho</button>
+                </section>
+                <section>
+                    <label>CEP: </label>
+                    <input type="number"/>
+                    <button>Calcular Frete</button>
+                </section>                
             </div>
         )
     }
