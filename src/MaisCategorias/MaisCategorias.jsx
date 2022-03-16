0b9be9314f1a137ba32750d2a9f957c658f1fe79
Jonathas Castilho <jonathascastilhoes@gmail.com>
2022-03-16 12:28:08 -0300

    Adicionando rotas para os links no Ver Mais Categorias

--- src/MaisCategorias/MaisCategorias.jsx
+++ src/MaisCategorias/MaisCategorias.jsx
@@ -1,10 +1,11 @@
+import { Link } from "@mui/material";
 import React from "react"
 import categoriasApi from "../categorias";
 
 function MaisCategorias (props) {
     return (
         <ul>
-            {categoriasApi.retorno.categorias.map(value => <li key={value.categoria.id}>{value.categoria.descricao}</li>)}
+            {categoriasApi.retorno.categorias.map(value => <li key={value.categoria.id}><a href={"/search?categoria=".concat(value.categoria.descricao)}>{value.categoria.descricao}</a></li>)}
         </ul>
     )
 }
